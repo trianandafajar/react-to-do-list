cf67f4f02a30a3468b4ecf1c382bac9b5ac50809
alvindioprakosa <alvindioprakosa@gmail.com>
2025-02-21 10:31:39 +0700

    Update Text

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="header">
       <div data-cy="header-background" className="container">
-        <h2 data-cy="header-title">TO DO LIST APP</h2>
+        <h2 data-cy="header-title">TO DO LIST APPLICATIONS</h2>
       </div>
     </div>
   );
